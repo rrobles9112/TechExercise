7389cc53f0156f35d1baf89e0a20e209b29852f3
richard.robles <richard.robles@globant.com>
2021-03-12 19:01:48 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from test import exportable_function
+
+
+
+print(exportable_functions())
---
+++ test.py
@@ -0,0 +1,12 @@
+print("hola mundo")
+a = 5 +2
+
+def exportable_function():
+    return "hola mundo"
+
+
+print("hello world")
+
+
+if __name__ == __name__:
+    exportable_function()
