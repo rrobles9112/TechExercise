ab6bebf64cc51ce44a30597efdfd4cdcebf62a4e
richard.robles <richard.robles@globant.com>
2021-05-24 20:56:15 -0500

    latest changes from exercises

--- test.py
+++ test.py
@@ -1,40 +1,13 @@
-print("hola mundo")
-a = 5 + 2
+from matplotlib.pyplot import text
 
 
-def exportable_function():
-    for i in range():
-        print(i)
-    return "hola mundo"
+def test(args):
+    for a in args:
+        print(a)
+        
 
 
-print("hello world")
+test(args=[1,2,3])
 
 
-def BracketCombinations(num):
-
-  combos = getCombos(num)
-
-  # code goes here
-  return len(combos)
-
-
-def getCombos(num, stri=""):
-  if len(stri) >= num * 2:
-    return [stri]
-  
-  combos = []
-
-  if stri.count("(") < num:
-    combos += getCombos(num, stri + "(")
-
-  if stri.count(")") < stri.count("("):
-    combos += getCombos(num, stri + ")")
-
-  return combos
-
-    
-  
-
-# keep this function call here 
-print(BracketCombinations(input()))
+ 
